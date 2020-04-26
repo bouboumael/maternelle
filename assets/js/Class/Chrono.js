9030479085240f971156833358cb736c6b9badf4
bouboumael <bouboumael@orange.fr>
2020-04-26 23:04:36 +0200

    refactoring and use POO

--- assets/js/Class/Chrono.js
+++ assets/js/Class/Chrono.js
@@ -1,5 +1,244 @@
 export default class Chrono {
 
+    _largeur
+    _distance
+    _pourcent
+    _debut
+    _timer
+    _reduct
+
+    constructor(course, route, snail, laitue) {
+        this._course = course
+        this._route = route
+        this._snail = snail
+        this._laitue = laitue
+        this._position = 0
+        this._time = 20
+        this._ms = 10
+    }
+
+    /**
+     * définition des variables de calcul
+     */
+    define(){
+        this.largeur = this.route.clientWidth
+        this.distance = this.largeur-100
+        this.pourcent = (this.ms*(this.distance/this.time))/1000
+        this.debut = (this.course.clientWidth-this.route.clientWidth)/2
+    }
+
+    /**
+     * start chrono
+     */
+    start(){
+        this.define()
+        this.timer = setInterval(this.move.bind(this), this.ms)
+    }
+
+    /**
+     * stop chrono
+     */
+    stop(){
+        clearInterval(this.timer)
+    }
+
+    /**
+     * reset des éléments du chrono
+     */
+    reset(){
+        this.stop()
+        clearInterval(this.reduct)
+        this.position = 0
+        this.snail.style.left = this.positionLeft('snail')
+        this.laitue.classList.remove('finish')
+        this.laitueDimension(true)
+    }
+
+    /**
+     * replacement des éléments après redimenssionnage de la fenetre
+     * @param course
+     * @param route
+     */
+    resize(course, route){
+        this.debut = (course.clientWidth-route.clientWidth)/2
+        this.course = course
+        this.route = route
+        this.snail.style.left = this.positionLeft('snail')
+        this.laitue.style.left = this.positionLeft('laitue')
+        this.laitue.style.top = this.course.offsetTop + ((this.route.height-this.laitue.height)/2) + "px"
+    }
+
+    /**
+     * animation du chrono escargot
+     */
+    move (){
+        if (this.laitue.style.visibility === ""){
+            this.snail.style.top = '-130px'
+            this.laitue.style.visibility = 'visible'
+            this.laitue.style.top = this.course.offsetTop + ((this.route.height-this.laitue.height)/2) + "px"
+            this.laitue.style.left = this.positionLeft('laitue')
+        }
+        if (this.position > this.largeur - (this.snail.clientWidth)){
+            this.stop()
+            this.laitue.classList.add('finish')
+            this.reduct = setInterval(function (){
+                if (this.laitue.clientWidth > 1){
+                    this.laitueDimension(false)
+                }else{
+                    clearInterval(this.reduct)
+                }
+            }.bind(this),100)
+        }else {
+            if (this.snail.style.display === '') {
+                this.snail.style.display = 'block'
+            }
+            this.snail.style.left = this.positionLeft('snail')
+            this.position = this.position + this.pourcent
+        }
+    }
+
+    //dimenssionnement de la laitue
+    laitueDimension (start){
+        if (start !== true){
+            this.laitue.style.width = (this.laitue.clientWidth - 2)+"px"
+            this.laitue.style.height = (this.laitue.clientHeight - 2)+"px"
+            this.laitue.style.left = this.positionLeft('laitue')
+            this.laitue.style.top = this.laitue.offsetTop + 1 + "px"
+        }else{
+            this.laitue.style.width = '100px'
+            this.laitue.style.height = '100px'
+            this.laitue.style.opacity = ""
+            this.laitue.style.top = this.course.offsetTop + ((this.route.height-this.laitue.height)/2) + "px"
+            this.laitue.style.left = this.positionLeft('laitue')
+        }
+    }
+
+    /**
+     * positionnement des éléments en css left
+     * @param e
+     * @returns {string}
+     */
+    positionLeft(e){
+        let left
+        switch (e) {
+            case 'snail':
+                left = this.debut + this.position+"px"
+                break
+            case 'laitue':
+                left = (this.course.offsetLeft + this.debut + this.route.clientWidth) - this.laitue.clientWidth +"px"
+                break
+            default:
+                break
+        }
+
+        return left
+
+    }
+
+
+    get course() {
+        return this._course;
+    }
+
+    set course(value) {
+        this._course = value;
+    }
+
+    get route() {
+        return this._route;
+    }
+
+    set route(value) {
+        this._route = value;
+    }
+
+    get snail() {
+        return this._snail;
+    }
+
+    set snail(value) {
+        this._snail = value;
+    }
+
+    get laitue() {
+        return this._laitue;
+    }
+
+    set laitue(value) {
+        this._laitue = value;
+    }
+
+    get largeur() {
+        return this._largeur;
+    }
+
+    set largeur(value) {
+        this._largeur = value;
+    }
+
+    get distance() {
+        return this._distance;
+    }
+
+    set distance(value) {
+        this._distance = value;
+    }
+
+    get pourcent() {
+        return this._pourcent;
+    }
+
+    set pourcent(value) {
+        this._pourcent = value;
+    }
+
+    get debut() {
+        return this._debut;
+    }
+
+    set debut(value) {
+        this._debut = value;
+    }
+
+    get position() {
+        return this._position;
+    }
+
+    set position(value) {
+        this._position = value;
+    }
+
+    get timer() {
+        return this._timer;
+    }
+
+    set timer(value) {
+        this._timer = value;
+    }
+
+    get time() {
+        return this._time;
+    }
+
+    set time(value) {
+        this._time = value;
+    }
+
+    get ms() {
+        return this._ms;
+    }
+
+    set ms(value) {
+        this._ms = value;
+    }
+
+    get reduct() {
+        return this._reduct;
+    }
+
+    set reduct(value) {
+        this._reduct = value;
+    }
 
 
 }
--- assets/js/alphabet.js
+++ assets/js/alphabet.js
@@ -1,4 +1,6 @@
 import Alphabet from "./Class/Alphabet.js"
+import Chrono from "./Class/Chrono.js";
+
 let snail = document.getElementById('snail')
 let route = document.getElementById('route')
 let course = document.getElementById('course')
@@ -6,23 +8,14 @@ let laitue = document.getElementById('laitue')
 let btn_debut = document.getElementById('btn-debut')
 let success = document.getElementById('success')
 let grille_lettre = document.getElementById('grille-lettre')
-let largeur
-let distance
-let time = 5 // temps de l'exercice en seconde
-let ms = 10 // fps
-let position = 0
-let pourcent
-let timer
-let debut
 let start = false
-let reduct
+let chrono = new Chrono(course, route, snail, laitue)
+
 
 
 //redimenssionement des elements avec la taille de la fenètre
 window.onresize = function () {
-    debut = (course.clientWidth-route.clientWidth)/2
-    snail.style.left = positionLeft('snail')
-    laitue.style.left = positionLeft('laitue')
+    chrono.resize(course, route)
 }
 
 //action du bouton
@@ -36,90 +29,14 @@ btn_debut.onclick = function(){
             course.style.display = 'block'
             grille_lettre.style.display = 'block'
             if (course.style.display === 'block'){
-                timer = setInterval(move, ms)
+                //timer = setInterval(move, ms)
+                chrono.start()
+                start = true
             }
             btn_debut.innerText = 'Lettre suivante'
         }else{
-            clearInterval(timer)
-            clearInterval(reduct)
-            position = 0
-            snail.style.left = positionLeft('snail')
-            laitue.classList.remove('finish')
-            laitueDimension(true)
-            timer = setInterval(move, ms)
+            chrono.reset()
+            chrono.start()
         }
     }
-}
-
-//animation du chrono escargot
-function move (){
-    start = true
-    largeur = route.clientWidth
-    distance = largeur-100
-    pourcent = (ms*(distance/time))/1000
-    debut = (course.clientWidth-route.clientWidth)/2
-    snail.style.top = "-130px"
-    laitue.style.top = course.offsetTop + 40 + "px"
-    laitue.style.left = positionLeft('laitue')
-
-    if (laitue.style.visibility === ""){
-        laitue.style.visibility = 'visible'
-    }
-
-    if (position > largeur - (snail.clientWidth)){
-        clearInterval(timer)
-        laitue.classList.add('finish')
-        reduct = setInterval(function (){
-            if (laitue.clientWidth > 1){
-                laitueDimension(false)
-            }else{
-                clearInterval(reduct)
-            }
-        },100)
-    }else{
-        if (snail.style.display === ''){
-            snail.style.display = 'block'
-        }
-        snail.style.left = positionLeft('snail')
-        position = position + pourcent
-    }
-}
-
-//dimenssionnement de la laitue
-function laitueDimension (s){
-    if (s !== true){
-        laitue.style.width = (laitue.clientWidth - 2)+"px"
-        laitue.style.height = (laitue.clientHeight - 2)+"px"
-        laitue.style.left = positionLeft('laitue')
-        laitue.style.top = laitue.offsetTop + 1 + "px"
-    }else{
-        laitue.style.width = '100px'
-        laitue.style.height = '100px'
-        laitue.style.opacity = ""
-        laitue.style.top = course.offsetTop + 40 + "px"
-        laitue.style.left = positionLeft('laitue')
-    }
-}
-
-//positionnement des éléments en css left
-function positionLeft(e){
-    let left
-    switch (e) {
-        case 'snail':
-            left = debut + position+"px"
-            break
-        case 'laitue':
-            left = (course.offsetLeft+ debut + route.clientWidth) - laitue.clientWidth +"px"
-            break
-        default:
-            break
-    }
-
-    return left
-
-}
-
-//remplissage de la grille des lettres
-function grilles() {
-
 }
